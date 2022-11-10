4eb22236b5a934e280639acfb438b39adee14571
Liangziqiang <1440584042@qq.com>
2022-11-10 11:05:54 +0800

    fv212e commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello git2!");
-        System.out.println("Hello 55556!");
+        System.out.println("Hello 555555555555555555556!");
 
     }
 
